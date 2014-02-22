6c9bfc7dc6e16a26984b0864084fefb116a32ad5
Christopher Arndt <SpotlightKid@users.noreply.github.com>
2014-02-22 17:40:32 +0000

    Keep sections from file 1; print info about differences to stderr; fix formatting of wrapped lines

--- accountmanager/accountdiff.py
+++ accountmanager/accountdiff.py
@@ -6,6 +6,10 @@ import optparse
 import os
 import sys
 
+
+def warn(msg, *args):
+    sys.stderr.write((msg % args) + '\n')
+
 class AccountDiff:
     """Print differences between two acount files."""
 
@@ -28,11 +32,21 @@ class AccountDiff:
         db2.optionsxform = str
         db2.read(args[1])
 
-        for section in sorted(db2.sections(), key=lambda x: x.upper()):
-            if db1.has_section(section):
-                self.compare_sections(db1, db2, section)
-            else:
+        sections = list(set(db1.sections() + db2.sections()))
+
+        for section in sorted(sections, key=lambda x: x.upper()):
+            if db1.has_section(section) and not db2.has_section(section):
+                warn("Section '%s' exists only in file 1.", section)
+                self.print_section(db1, section)
+            elif db2.has_section(section) and not db1.has_section(section):
+                warn("Section '%s' exists only in file 2.", section)
                 self.print_section(db2, section)
+            else:
+                warn("Section '%s' exists in file 1 and 2.", section)
+                if self.compare_sections(db1, db2, section):
+                    warn("Options in section '%s' differ bewteen file 1 and 2."
+                        , section)
+
 
     def parse_options(self, args=None):
         self.optparser = optparse.OptionParser(prog=self.name,
@@ -66,20 +80,30 @@ class AccountDiff:
     def compare_sections(self, db1, db2, section):
         print("[%s]" % section)
 
-        options = list(set(db1.options(section) + db2.options(section)))
+        db1_options = db1.options(section)
+        db2_options = db2.options(section)
+        options = list(set(db2_options + db2_options))
+        diff = True
 
         for option in sorted(options):
             if not db1.has_option(section, option):
-                print("A %s: %s" % (option, db2.get(section, option)))
+                value = db2.get(section, option).replace('\n', ' ')
+                print("A %s: %s" % (option, value))
             elif not db2.has_option(section, option):
-                print("D %s: %s" % (option, db1.get(section, option)))
+                value = db1.get(section, option).replace('\n', ' ')
+                print("D %s: %s" % (option, value))
             elif db1.get(section, option) != db2.get(section, option):
-                print("- %s: %s" % (option, db1.get(section, option)))
-                print("M %s: %s" % (option, db2.get(section, option)))
+                value = db1.get(section, option).replace('\n', ' ')
+                print("- %s: %s" % (option, value))
+                value = db2.get(section, option).replace('\n', ' ')
+                print("M %s: %s" % (option, value))
             else:
-                print("%s: %s" % (option, db1.get(section, option)))
+                value = db1.get(section, option).replace('\n', ' ')
+                print("%s: %s" % (option, value))
+                diff = False
 
         print("")
+        return diff
 
 if __name__ == '__main__':
     ad = AccountDiff()
