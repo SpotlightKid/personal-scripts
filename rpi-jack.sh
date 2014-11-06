85b015d3f01b53f376b66e69bc3d4690f03a6ec0
Christopher Arndt <SpotlightKid@users.noreply.github.com>
2014-11-06 14:03:31 +0000

    Add 'rpi-jack.sh' script; ignore 'test.htpasswd'

---
+++ rpi-jack.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+AUDIODEV="hw:1"
+MIDIDEV="FastTrack Pro"
+SAMPLERATE=44100
+CHANNELS=2
+GAIN="0.5"
+JACK="true"
+SOUNDFONT="${1:-/usr/share/sounds/sf2/FluidR3_GM.sf2}"
+
+if [ "x$JACK" = "xtrue" ]; then
+    jackd -R -P95 -p 32 -d dummy -C 0 -P $CHANNELS -r $SAMPLERATE &
+    sleep 1
+    alsa_out -d "$AUDIODEV" -c $CHANNELS -r $SAMPLERATE >&/dev/null &
+    sleep 1
+    fluidsynth -s -i -L $(($CHANNELS / 2)) -r $SAMPLERATE -g "$GAIN" \
+        -a jack -m alsa_seq -p fluidsynth1 \
+        "$SOUNDFONT" &
+else
+    fluidsynth -s -i -L $(($CHANNELS / 2)) -r $SAMPLERATE -g "$GAIN" \
+       -a alsa -o "audio.alsa.device=$AUDIODEV" -m alsa_seq -p fluidsynth1 \
+       "$SOUNDFONT" &
+fi
+
+sleep 2
+aconnect "$MIDIDEV:0" "fluidsynth1:0"
