57061d107de87f058db8f4665f66257b699fd53b
carndt <carndt@d91bdeee-4e13-0410-98cf-97065b31a653>
2015-11-01 16:10:41 +0000

    Add script to sync Kurzweil PC3(K) files from/to usb drive

---
+++ projects/scripts/synckurzweil.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# trailing slashes are important!
+SRC="/run/media/chris/UsbStick16G/kurzweil/"
+DEST="/home/chris/Dokumente/kurzweil/"
+
+if [[ "$1" = "-f" ]]; then
+    DRY_RUN=
+    shift
+else
+    echo "Running in dry mode..."
+    DRY_RUN="-n"
+fi
+
+if [[ $1 = "-r" ]]; then
+    DEST2="$DEST"
+    DEST="$SRC"
+    SRC="$DEST2"
+    shift
+fi
+
+echo "Syncing contents of $SRC to $DEST..."
+
+rsync $DRY_RUN -rv --checksum \
+    --exclude 00archives \
+    --exclude .DS_Store \
+    --exclude "*.png" \
+    --exclude "*.rst" \
+    --exclude "*.rar" \
+    --exclude "*.zip" \
+    "$@" \
+    "$SRC" "$DEST"
