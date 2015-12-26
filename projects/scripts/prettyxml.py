bf47ea562bfa727c7e627231797dcf4ded189d58
carndt <carndt@d91bdeee-4e13-0410-98cf-97065b31a653>
2015-12-26 15:51:22 +0000

    Python 3 compatibility

--- projects/scripts/prettyxml.py
+++ projects/scripts/prettyxml.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
+
 import sys
 from xml.dom.minidom import parse
 
 d = parse(sys.argv[1])
-sys.stdout.write(d.toprettyxml(indent="  ", encoding="utf-8"))
+print(d.toprettyxml(indent="  ", encoding="utf-8").decode('utf-8'))
