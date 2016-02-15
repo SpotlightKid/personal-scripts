471f954556f52bff951f6e4069efc37ce79627c2
Christopher Arndt <SpotlightKid@users.noreply.github.com>
2016-02-15 13:42:50 +0000

    Add script abc2pdf

---
+++ abc2pdf.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Usage: abc2pdf [options] <file.abc>"
+    return 2
+else
+    abc="${@: -1}"
+    if [ ! -e "$abc" ]; then
+        echo "Input file '"$abc"' does not exists."
+        return 1
+    fi
+fi
+
+# construct output filenames from input options
+basefn="${abc##*/}"
+basen="${basefn%.*}"
+TMP="${TMP:-/tmp}"
+PS="$(mktemp "$TMP/abc2pdf-XXXXXX.ps")"
+PDF="$basen.pdf"
+
+# generate PostScript with abcm2ps
+abcm2ps $ABCM2PS_OPTS -O "$PS" "$@"; ret=$?
+
+if [ $ret -ne 0 ]; then
+    echo "Warning: error in conversion of ABC file to PostScript."
+    echo "abcm2ps returned $ret."
+fi
+
+if [ -e "$PS" ]; then
+    ps2pdf "$PS" "$PDF"; ret=$?
+    rm -f "$PS"
+fi
+
+exit $ret
