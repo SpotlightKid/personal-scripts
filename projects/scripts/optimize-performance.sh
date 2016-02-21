2d9f0a925de3450da5539d51ee1221ceef7178cb
carndt <carndt@d91bdeee-4e13-0410-98cf-97065b31a653>
2016-02-21 18:27:10 +0000

    Add script to optimize performance for audio work

---
+++ projects/scripts/optimize-performance.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# optimize-performance.sh
+#
+
+action="$1:-start"
+
+case "$action" in
+    start)
+        # Set VM swapiness for improved performance
+        sudo /sbin/sysctl -q -w vm.swappiness=10
+
+        # Set CPU to performance
+        sudo cpupower frequency-set -g performance
+
+        # Turn off WiFi
+        sudo iwconfig wlan0 txpower off
+
+        # Disable screen blanking
+        xset -dpms; xset s off
+        ;;
+    stop)
+        # Set VM swapiness to default
+        sudo /sbin/sysctl -q -w vm.swappiness=60
+
+        # Set CPU to powersave
+        sudo cpupower frequency-set -g powersave
+
+        # Turn on WiFi
+        sudo iwconfig wlan0 txpower on
+
+        # Enable screen blanking
+        xset +dpms; xset s on
+esac
+
+exit 0
