371b8392b64d80b051ef02a574847ee319e3879d
carndt <carndt@d91bdeee-4e13-0410-98cf-97065b31a653>
2019-01-24 15:01:50 +0000

    Add readme for accountdb script

--- projects/scripts/accountmanager/xmltoini.py
+++ projects/scripts/accountmanager/xmltoini.py
@@ -1,9 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Convert a Thunderbird password export XML file into an INI style file for accountdb.py."""
+
+import sys
 from xml.etree import ElementTree as ET
-d = ET.parse('minimax/tb_pw.xml')
+
+if len(sys.argv) > 1:
+    infile = sys.argv[0]
+else:
+    infile = 'tb_pw.xml'
+
+d = ET.parse(infile)
 entries = d.find('entries')
+
 for el in entries.findall('entry'):
-    print "[%s]" % el.get('host')
-    print "url: %s" % el.get('host')
-    print "user: %s" % el.get('user')
-    print "password: %s" % el.get('password')
-    print
+    print("[%s]" % el.get('host'))
+    print("url: %s" % el.get('host'))
+    print("user: %s" % el.get('user'))
+    print("password: %s" % el.get('password'))
+    print('')
