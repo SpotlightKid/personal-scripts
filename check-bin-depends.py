0eb7a09580e5565ad8948f0c3728238d36582e76
Christopher Arndt <SpotlightKid@users.noreply.github.com>
2019-01-24 14:53:11 +0000

    Put module level code into a main function

--- check-bin-depends.py
+++ check-bin-depends.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Hacky script to determine redundant dependencies of an AUR package."""
 
 import sys
 from collections import defaultdict
@@ -7,7 +8,9 @@ from functools import partial
 from subprocess import DEVNULL, PIPE, run, STDOUT
 from pprint import pprint
 
+
 err = partial(print, file=sys.stderr)
+
 
 def pipe(cmd, input='', stderr=None, **kwargs):
     kwargs['stdout'] = PIPE
@@ -17,60 +20,65 @@ def pipe(cmd, input='', stderr=None, **kwargs):
     return (res.stdout, res.stderr) if stderr == 'capture' else res.stdout
 
 
-depends = set()
-base_devel = set(line.strip() for line in pipe(['pacman', '-Qqg', 'base-devel']).splitlines())
-#err("\n".join(sorted(base_devel)))
-for pkg in base_devel:
-    depends.update(p.strip() for p in pipe(['pactree', '-l', pkg]).splitlines())
-depends.update(base_devel)
+def main(args):
+    depends = set()
+    base_devel = set(line.strip() for line in pipe(['pacman', '-Qqg', 'base-devel']).splitlines())
+    #err("\n".join(sorted(base_devel)))
+    for pkg in base_devel:
+        depends.update(p.strip() for p in pipe(['pactree', '-l', pkg]).splitlines())
+    depends.update(base_devel)
 
-for fn in sys.argv[1:]:
-    libs = set()
-    for line in pipe(['ldd', fn]).splitlines():
-        line = line.strip()
-        if line and not (line.startswith('linux-vdso') or 'ld-linux' in line):
-            libs.add(line.split()[2])
+    for fn in args:
+        libs = set()
+        for line in pipe(['ldd', fn]).splitlines():
+            line = line.strip()
+            if line and not (line.startswith('linux-vdso') or 'ld-linux' in line):
+                libs.add(line.split()[2])
 
-#err("\n".join(sorted(libs)))
+    #err("\n".join(sorted(libs)))
 
-err("\nCollecting packages...\n")
-packages = set()
-for path in libs:
-    package = pipe(['pkgfile', path]).splitlines()[0].strip()
-    if package:
-        #err("{} => '{}'".format(lib, package))
-        package = package.split('/')[1]
-        if package in depends:
-            err("Package '{}' in 'base-devel' group (and dependencies). Ignoring it.".format(package))
+    err("\nCollecting packages...\n")
+    packages = set()
+    for path in libs:
+        package = pipe(['pkgfile', path]).splitlines()[0].strip()
+        if package:
+            #err("{} => '{}'".format(lib, package))
+            package = package.split('/')[1]
+            if package in depends:
+                err("Package '{}' in 'base-devel' group (and dependencies). "
+                    "Ignoring it.".format(package))
+            else:
+                packages.add(package)
         else:
-            packages.add(package)
-    else:
-        err("Library file '{}' is not owned by any package.".format(path))
+            err("Library file '{}' is not owned by any package.".format(path))
 
-err("\nCollecting dependencies...\n")
-depends = defaultdict(set)
-for pkg in sorted(packages):
-    for dep in set(p.strip() for p in pipe(['pactree', '-l', pkg]).splitlines()):
-        if dep != pkg:
-            depends[dep].add(pkg)
-
-#pprint(depends)
-
-
-maxcount = 10
-err("\nRemoving indirect dependencies...\n")
-while True:
-    toremove = set()
+    err("\nCollecting dependencies...\n")
+    depends = defaultdict(set)
     for pkg in sorted(packages):
-        if depends[pkg]:
-            toremove.add(pkg)
-            err("Dependency '{}' already included by packages '{}'. Removing it.".format(pkg, ", ".join(depends[pkg])))
+        for dep in set(p.strip() for p in pipe(['pactree', '-l', pkg]).splitlines()):
+            if dep != pkg:
+                depends[dep].add(pkg)
 
-    maxcount -= 1
-    if not toremove or not packages or maxcount <= 0:
-        break
+    #pprint(depends)
 
-    packages.difference_update(toremove)
+    maxcount = 10
+    err("\nRemoving indirect dependencies...\n")
+    while True:
+        toremove = set()
+        for pkg in sorted(packages):
+            if depends[pkg]:
+                toremove.add(pkg)
+                err("Dependency '{}' already included by packages '{}'. "
+                    "Removing it.".format(pkg, ", ".join(depends[pkg])))
+
+        maxcount -= 1
+        if not toremove or not packages or maxcount <= 0:
+            break
+
+        packages.difference_update(toremove)
+
+    print("depends: ({})".format(" ".join("'{}'".format(pkg) for pkg in sorted(packages))))
 
 
-print("depends: ({})".format(" ".join("'{}'".format(pkg) for pkg in sorted(packages))))
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]) or 0)
