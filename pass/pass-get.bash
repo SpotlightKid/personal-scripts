6e3efeceb0c62b99a1a5be05e4fe874b8df097af
Christopher Arndt <chris@chrisarndt.de>
2019-11-16 07:08:35 +0100

    Add pass-get pass(1) extension
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

---
+++ pass/pass-get.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+path="$1"
+shift
+passfile="$PREFIX/$path.gpg"
+check_sneaky_paths "$path"
+
+if [[ "$1" = "-c" ]]; then
+    clipboard=1
+    shift
+fi
+
+if [[ -f $passfile ]]; then
+    if [[ -n "$1" ]]; then
+        key="$1"
+        $GPG -d "${GPG_OPTS[@]}" "$passfile" | tail -n +2 | while read line; do
+            if echo -n "$line" | grep -q ^$key'\s*[:=]'; then
+                line="$(echo $line | sed -e 's/^'$key'\s*[:=]\s*//')"
+                if [[ -n "$clipboard" ]]; then
+                    clip "$line"
+                else
+                    echo "$line"
+                fi
+            fi
+        done
+    else
+        $GPG -d "${GPG_OPTS[@]}" "$passfile" | tail -n +2 || exit $?
+    fi
+elif [[ -z $path ]]; then
+    die ""
+else
+    die "Error: $path is not in the password store."
+fi
