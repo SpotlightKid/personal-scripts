6f350f1276662204d5d8e4a8ecd80368105ea97b
Christopher Arndt <chris@chrisarndt.de>
2022-02-20 18:59:41 +0100

    feat: improve sudo-askpass.sh script
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- sudo-askpass.sh
+++ sudo-askpass.sh
@@ -1,11 +1,33 @@
-#!/bin/sh
+#!/bin/bash
 #
-# sudo-askpass - Use zenity to ask for sudo password
+# sudo-askpass - Use yad or zenity to ask for sudo password
 #
-# To use, set add this to your shell profile:
+# To use, add this to your shell profile:
 #
 # export SUDO_ASKPASS=sudo-askpass
 #
 # and then call sudo with the "-A" option
 
-exec zenity --password --title "Enter password for $USER"
+PROMPT="Authentication required for $USER"
+
+if which yad &>/dev/null; then
+    exec yad --entry --entry-label "Password" --hide-text --image=password --window-icon=dialog-password --text="$PROMPT" --title=Authentication --center
+elif which zenity &>/dev/null; then
+    exec zenity --password --title "$PROMPT"
+elif which kdialog &>/dev/null; then
+    exec kdialog --title "Authentication" --password "$PROMPT"
+else
+    STTY_SAVE="$(stty -g)"
+
+    abort() {
+        stty $STTY_SAVE
+    }
+
+    trap abort EXIT
+    echo -n "Enter password for $USER: " > /dev/stderr
+    stty -echo
+    read PASSWORD
+    echo
+    stty $STTY_SAVE
+    echo $PASSWORD
+fi
